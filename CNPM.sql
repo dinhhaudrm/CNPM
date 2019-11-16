39182f25c1e84140ae0da527ff5317e0bcfed60a
dinhhaudrm <57657727+dinhhaudrm@users.noreply.github.com>
2019-11-16 12:58:54 +0700

    Update CNPM.sql

--- CNPM.sql
+++ CNPM.sql
@@ -94,8 +94,47 @@ create table BangLuong
 )
 go
 --Thêm nhân viên--
---CREATE TRIGGER ADD_Nhanvien
---on dbo.NhanVien
---after insert 
---as cái này anh Hậu viết xong cho chạy luôn rồi test các thứ coi đúng không
---xong rồi nếu ok thì tô đen lại hết rôi comment
+create proc ThemNV
+	@MaNV varchar(100),
+	@HoTen nvarchar(100),
+	@NgaySinh date,
+	@Phai nvarchar(10),
+	@SDT varchar(11),
+	@DiaChi nvarchar(100),
+	@MaHopDong varchar(100),
+	@Pass varbinary(max),
+	@ChucVu varchar(10)
+as
+begin
+	declare @defaultPass varbinary(max)=  hashbytes('SHA2_512', '1000.'+@Pass)
+		insert into NhanVien
+		values (@MaNV,@HoTen,@NgaySinh,@Phai,@SDT,@DiaChi,@MaHopDong)
+
+		insert into TaiKhoan
+		values (@MaNV,@defaultPass,@ChucVu)
+end
+go
+--Tìm nhân viên (gồm mã và tên nhân viên --
+create function TimNV(@MaNV varchar(100),@HoTen nvarchar(100))
+returns table
+as
+return (select * from NhanVien where MaNV like ('%'+ @MaNV +'%')  and HoTen like ( '%'+ @HoTen +'%') )
+go
+-- Lưu lại giờ vào  ca --
+create proc KiemTra @MaNV varchar(100), @Ngay date, @KT time,@NgayLam date,@Tre time
+as
+begin
+	declare @KTLoai time =
+	(
+		select GioVao from ChiTietChamCong where MaNV=@MaNV and day(Ngay)=day(@NgayLam) and YEAR(Ngay)=YEAR(@NgayLam) and MONTH(Ngay)=MONTH(@NgayLam)
+	)
+	if @KTLoai is null
+	begin
+		update ChiTietChamCong
+		set MaNV=@MaNV,
+		Ngay=@Ngay,
+		GioVao=@KT,
+		Tre=@Tre
+	end
+end
+go
